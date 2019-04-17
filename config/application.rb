383a677892901050fcdc3c859ebecd568d2c187f
edzye101 <edzye101@gmail.com>
2019-04-18 00:26:52 +0100

    changed the config name to synbio guru

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module OxbridgeIo
+module SynBio
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
-    config.application_name = 'Oxbridge.io'
+    config.application_name = 'SynBio.Guru'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     # config.assets.paths << Rails.root.join('node_modules')
